e717c0b9c345d5ac5f34793d518f2c899b3777e2
MahmoudFadel96 <Mahmoud.Fadel.1996@gmail.com>
2022-05-07 07:14:05 +0200

    Unit 2

---
+++ Unit_2/Lesson_3/Ex2/main.c
@@ -0,0 +1,12 @@
+/*
+ * main.c
+ *
+ *  Created on: May 7, 2022
+ *      Author: Dell
+ */
+
+
+#include"Stdio.h"
+void main(){
+	printf("Hello Ass2");
+}
---
+++ Unit_2/Lesson_3/Ex3/main.c
@@ -0,0 +1,11 @@
+/*
+ * main.c
+ *
+ *  Created on: May 7, 2022
+ *      Author: Dell
+ */
+
+#include"Stdio.h"
+void main(){
+	printf("Hello Ass3");
+}
